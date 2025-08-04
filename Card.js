aace466c568870fd20afd0f669cd1d42e9119f8a
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2025-08-04 01:05:07 -0400

    create Card and place it in middle of Canvas to test out card flipping

--- Card.js
+++ Card.js
@@ -1,4 +1,4 @@
-class Card extends Sprite {
+class Card /*extends Sprite*/ {
 
     constructor(x, y, defaultImg, turnImg) {
         if ((!(defaultImg instanceof p5.Image))) {
@@ -9,22 +9,20 @@ class Card extends Sprite {
             throw new TypeError("turnImg must be of type p5.Image.");
         }
 
-
-        super(x, y, 75, 75, KIN);
         this.defaultImg = defaultImg;
         this.turnImg = turnImg;
-        this.img = this.defaultImg;
+        this.sprite = new Sprite(this.defaultImg, x, y, 75, 75, KIN);
     }
 
     flip() {
-        (this.img == this.defaultImg) ? this.img = turnImg : this.img = this.defaultImg;
+        (this.sprite.img == this.defaultImg) ? this.sprite.img = this.turnImg : this.sprite.img = this.defaultImg;
     }
 
     matches(otherCard) {
         if ((!(otherCard instanceof Card))) {
             throw new TypeError("otherCard must be of type Card.");
         }
-        return this.img == otherCard.img;
+        return this.sprite.img == otherCard.sprite.img;
     }
 
 }
--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function preload() {
+var cardBack;
+var macNCheese
+var card1;
 
+function preload() {
+    cardBack = loadImage("serving-plate-w-lid.jpg");
+    macNCheese = loadImage("mac-n-cheese.png");
 }
 
 function setup() {
+    cardBack.resize(75, 75);
+    macNCheese.resize(75, 75);
     createCanvas(400, 400);
+    card1 = new Card(width / 2, height / 2, cardBack, macNCheese);
+    //if (card1.sprite.mouse.presses)
+        //card1.flip();
 }
 
 function draw() {
