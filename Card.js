99cfc2524a1bd780b919840be2ef9eaa526e8da6
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2025-08-03 23:34:31 -0400

    creating Card class and importing its script

---
+++ Card.js
@@ -0,0 +1,23 @@
+class Card extends Sprite {
+
+    constructor(x, y, defaultImg, turnImg) {
+        if ((!(defaultImg instanceof p5.Image))) {
+            throw new TypeError("defaultImg must be of type p5.Image.");
+        }
+
+        if ((!(turnImg instanceof p5.Image))) {
+            throw new TypeError("turnImg must be of type p5.Image.");
+        }
+
+
+        super(x, y, 75, 75, KIN);
+        this.defaultImg = defaultImg;
+        this.turnImg = turnImg;
+        this.img = this.defaultImg;
+    }
+
+    flip() {
+        (this.img == this.defaultImg) ? this.img = turnImg : this.img = this.defaultImg;
+    }
+
+}
