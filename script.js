b1fd91cd14094da0445206c8a2751100c496ac87
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2026-05-22 19:50:48 -0400

    adding cards array - randomizing the images on each card and allowing all the cards to be flipped

--- script.js
+++ script.js
@@ -19,9 +19,11 @@ function setup() {
     createCanvas(400, 400);
     imgs = [
         [macNCheese, _4CheeseMac, loadedWaffFries, nathansMeal],
-        []
+        [cheeseDogs, beef_taco, sugarDonuts, chalupa]
     ];
-    cards = [[new Card(75, 75, cardBack, macNCheese)]];
+    cards = [[new Card(75, 75, cardBack, randomizeCardImage(random(random(imgs)))), new Card(175, 75, cardBack, randomizeCardImage(random(random(imgs)))),
+        new Card(275, 75, cardBack, randomizeCardImage(random(random(imgs)))), new Card(375, 75, cardBack, randomizeCardImage(random(random(imgs))))
+    ]];
     /*card1 = new Card(width / 2 - width / 4, height / 2, cardBack, macNCheese);
     card2 = new Card(width / 2, height / 2, cardBack, loadedWaffFries);
     card3 = new Card(width / 2 + width / 4, height / 2, cardBack, cheeseDogs);*/
@@ -29,10 +31,32 @@ function setup() {
 
 function draw() {
     background("pink");
+    for (var r = 0; r < cards.length; r++) {
+        for (var c = 0; c < cards[0].length; c++) {
+          if (cards[r][c].sprite.mouse.pressed())
+            cards[r][c].flip(); 
+        }
+    }
     /*if (card1.sprite.mouse.pressed())
         card1.flip();
     if (card2.sprite.mouse.pressed())
         card2.flip();
     if (card3.sprite.mouse.pressed())
         card3.flip();*/
+}
+
+function randomizeCardImage(img) {
+    var timesRandomized = 0;
+    if (timesRandomized < 2) {
+        if ((!(img instanceof p5.Image)))
+            throw new TypeError("img must be of type p5.Image.");
+        var randomImg = random(random(imgs));
+        if (randomImg == img) {
+            timesRandomized++;
+        }
+        return randomImg;
+    }
+    else {
+        randomizeCardImage(img);
+    }
 }
